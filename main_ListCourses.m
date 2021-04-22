9f270cca3059a4f369484f24be01f5d86fd5acb8
GGQ_TP2101 <gqguan@hotmail.com>
2021-04-22 10:48:10 +0800

    添加脚本main_ListCourses

---
+++ main_ListCourses.m
@@ -0,0 +1,22 @@
+%% 在MS-Word中制表输出“毕业要求指标点支撑课程”
+%
+% by Dr. Guan Guoqiang @ SCUT on 2021/4/22
+
+%% 初始化
+clear
+% 载入必修和选修课程表
+load('database.mat', 'db_Curriculum2021a', 'db_Curriculum2021b')
+
+%% 整理列出
+% 从database.mat中载入db_Indicators
+outTab = SupportCourses(db_Curriculum2021a,db_Curriculum2021b);
+
+%% 制表输出结果
+% 表头
+TabHead = {'毕业要求观测点','支撑必修课','支撑选修课'};
+% 表类型
+TabType = '指标点支撑课程列表';
+% 输出文件名
+TabFilename = '2021年能源化工专业毕业要求指标点支撑课程表';
+% 输出MS-Word文件
+Tab2Word(outTab,TabHead,TabType,TabFilename)
