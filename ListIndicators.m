d2fb818e164b802948b6d3ad96f60eb53a9f45c7
GGQ_TP2101 <gqguan@hotmail.com>
2021-04-26 21:29:24 +0800

    [notes] 添加注释

--- ListIndicators.m
+++ ListIndicators.m
@@ -1,14 +1,21 @@
 %% 在课程列表后生成所支撑的毕业要求指标编号栏
+%
+% by Dr. Guan Guoqiang @ SCUT on 2021/4/26
 
+%% 初始化
 clear
-
-load('database.mat','db_Curriculum2019a')
-load('database.mat','db_Curriculum2019b')
+cCourseList = 'db_Curriculum2019a';
+sCourseList = 'db_Curriculum2019b';
+% 载入必修和选修课程
+load('database.mat',cCourseList)
+load('database.mat',sCourseList)
+% 载入指标点
 load('database.mat','db_Indicators')
 
+%% 按必修课表列出相应支撑的指标点编号
 supportIndicator = string;
-for iCourse = 1:height(db_Curriculum2019a)
-    idx = logical(db_Curriculum2019a.ReqMatrix(iCourse,:));
+for iCourse = 1:height(eval(cCourseList))
+    idx = logical(eval(cCourseList).ReqMatrix(iCourse,:));
     UniNums = db_Indicators.UniNum(idx);
     content = string;
     for i = 1:length(UniNums)
@@ -20,12 +27,13 @@ for iCourse = 1:height(db_Curriculum2019a)
     end
     supportIndicator(iCourse,1) = content;
 end
+cList1 = eval(cCourseList);
+cList1 = [cList1(:,2:3),table(supportIndicator)];
 
-cList1 = [db_Curriculum2019a(:,2:3),table(supportIndicator)];
-
+%% 按选修课表列出相应支撑的指标点编号
 supportIndicator = string;
-for iCourse = 1:height(db_Curriculum2019b)
-    idx = logical(db_Curriculum2019b.ReqMatrix(iCourse,:));
+for iCourse = 1:height(eval(sCourseList))
+    idx = logical(eval(sCourseList).ReqMatrix(iCourse,:));
     UniNums = db_Indicators.UniNum(idx);
     content = string;
     for i = 1:length(UniNums)
@@ -37,7 +45,8 @@ for iCourse = 1:height(db_Curriculum2019b)
     end
     supportIndicator(iCourse,1) = content;
 end
+cList2 = eval(sCourseList);
+cList2 = [cList2(:,2:3),table(supportIndicator)];
 
-cList2 = [db_Curriculum2019b(:,2:3),table(supportIndicator)];
-
+%% 合并必修和选修课表结果
 cList = [cList1;cList2];
