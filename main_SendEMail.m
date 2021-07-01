49aae164cb0429fbb0519f75396fab2f75669302
GGQ_TP2101 <gqguan@hotmail.com>
2021-07-01 17:56:41 +0800

    [feature] 采用outlook的ActiveX组件发邮件避免用sendmail通过smtp.scut.edu.cn并发多个邮件造成错误！

--- main_SendEMail.m
+++ main_SendEMail.m
@@ -13,35 +13,35 @@ courses = db_Curriculum2021a(idx,:);
 %% 对各课程列出其支撑的指标
 supportIndicator = string;
 for iCourse = 1:height(courses)
-    fprintf('课程《%s》负责人邮件（%s）正在发送……',courses.Name(iCourse),courses.Email(iCourse))
+    fprintf('课程《%s》负责人邮件（To: %s）正在发送……',courses.Name(iCourse),courses.Email(iCourse))
     idx = logical(courses.ReqMatrix(iCourse,:));
     UniNums = db_Indicators2021.UniNum(idx);
     Specs = db_Indicators2021.Spec(idx);
     content = string;
     for i = 1:length(UniNums)
         if i == 1
-            content = sprintf('%s\n', strcat(UniNums{i},'：',Specs{i}));
+            content = sprintf('<p>%s</p>', strcat(UniNums{i},'：',Specs{i}));
         else
-            content = sprintf('%s%s\n', content, strcat(UniNums{i},'：',Specs{i}));
+            content = [content,sprintf('<p>%s</p>', content, strcat(UniNums{i},'：',Specs{i}))];
         end
     end
     supportIndicator(iCourse,1) = content;
     % 邮件内容
-    ctext = sprintf('您好！\n');
+    ctext = sprintf('<p>您好！</p>');
     ctext = [ctext,sprintf('根据专业认证进校专家意见对能源化工专业的毕业要求课程支撑矩阵进行了简化，为此请您核对所负责的能源化工专业课程《%s》支撑的指标点如下：\n',courses.Name(iCourse))];
     ctext = [ctext,content];
-    ctext = [ctext,sprintf('后续请参照上列的指标点进行课程达成度分析和教纲修改。若有任何问题，请与我联系！\n')];
-    ctext = [ctext,sprintf('礼\n')];
-    ctext = [ctext,sprintf('关国强\n')];
-    ctext = [ctext,sprintf('%s\n',char(datetime('now')))];
-    ctext = [ctext,sprintf('备注：\n')];
-    ctext = [ctext,sprintf('公共教学团队若指定专门负责的老师，烦请转发该邮件给相关老师，谢谢！\n')];
+    ctext = [ctext,sprintf('<p>后续请参照上列的指标点进行课程达成度分析和教纲修改。若有任何问题，请与我联系！</p>')];
+    ctext = [ctext,sprintf('<p>礼</p>')];
+    ctext = [ctext,sprintf('<p>关国强<p>')];
+    ctext = [ctext,sprintf('<p>%s</p>',char(datetime('now')))];
+    ctext = [ctext,sprintf('<p>备注：</p>')];
+    ctext = [ctext,sprintf('<p>公共教学团队若指定专门负责的老师，烦请转发该邮件给相关老师，谢谢！</p>')];
     checksum = Simulink.getFileChecksum('main_SendEMail.m');
-    ctext = [ctext,sprintf('（邮件生成程序main_SendEMail.m，文件校验码：%s）\n',checksum(end-3:end))];
+    ctext = [ctext,sprintf('<p>（邮件生成程序main_SendEMail.m，文件校验码：%s）</p>',checksum(end-3:end))];
     % 收件人
     recipient = strsplit(courses.Email(iCourse),'; ');
     % 邮件主题
     subject = sprintf('能源化工专业必修课《%s》支撑指标点核对',courses.Name(iCourse));
-    email(recipient, subject, ctext)
+    sendolmail(recipient, subject, ctext)
     fprintf('完成！\n')
 end
